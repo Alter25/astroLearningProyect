2348e57c43a523ba7aa97cd1044f710dec10a958
Alter <jf.lozano.n@gmail.com>
2026-01-28 22:09:48 -0600

    feat: sidebar functionality done (style still in progress)

--- src/components/react/Sidebar.tsx
+++ src/components/react/Sidebar.tsx
@@ -1,9 +1,30 @@
 import { useState } from "react";
+import type { ReactNode } from "react";
 import type { Link } from "../../types/types";
+import { isAborted } from "astro:schema";
 
 interface Props {
   title: string;
   links?: Link[];
+}
+
+function VisibleButton({
+  children,
+  f,
+}: {
+  children: ReactNode;
+  f?: () => void;
+}) {
+  return (
+    <div className="h-full">
+      <button
+        className="text-2xl text-emerald-500 h-full hover:cursor-pointer border"
+        onClick={f}
+      >
+        {children}
+      </button>
+    </div>
+  );
 }
 
 export default function Sidebar({ title, links }: Props) {
@@ -12,27 +33,35 @@ export default function Sidebar({ title, links }: Props) {
     setIsVisible(!isVisible);
     console.log(isVisible);
   };
+  const visibleText = () => {
+    return isVisible ? "<" : ">";
+  };
+
   return (
-    <aside className={`${isVisible ? "w-50" : "w-fit"} h-full border`}>
-      <div className="h-full flex items-center gap-0">
-        <nav>
-          <ul>
-            {links &&
-              links.map((link) => {
-                return ( 
-                  <li>
-                    <a href={link.href}>{link.text}</a>
-                  </li>
-                );
-              })}
-          </ul>
-        </nav>
-        <div className="h-full">
-          <button onClick={onClick} className="hover:cursor-pointer h-full">
-            &gt;
-          </button>
+    <aside className="w-fit h-full border">
+      {isVisible ? (
+        <div className="h-full w-50 flex justify-end">
+          <nav className="mx-auto mt-4">
+            <ul>
+              {links &&
+                links.map((link) => {
+                  return (
+                    <li>
+                      <a href={link.href}>{link.text}</a>
+                    </li>
+                  );
+                })}
+            </ul>
+          </nav>
+          <VisibleButton f={() => setIsVisible(!isVisible)}>
+            {visibleText()}
+          </VisibleButton>
         </div>
-      </div>
+      ) : (
+        <VisibleButton f={() => setIsVisible(!isVisible)}>
+          {visibleText()}
+        </VisibleButton>
+      )}
     </aside>
   );
 }
