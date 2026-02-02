6ee2f8e6e90e9cbd9a89ed5a9f5d2d6ad0c2d458
Alter <jf.lozano.n@gmail.com>
2026-02-02 05:32:18 -0600

    feature: sidebar motion added

--- src/components/react/Sidebar.tsx
+++ src/components/react/Sidebar.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import type { Link } from "../../types/types";
 import ToggleList from "./ToggleList";
 import { useStoreBar } from "../../store/store";
@@ -9,27 +10,42 @@ interface Props {
 
 export default function Sidebar({ title = "sideBar", links }: Props) {
   const { show, toggle } = useStoreBar();
-  return show ? (
-    <aside className="min-w-60 flex flex-col items-center py-2 px-3">
-      <div className="w-full flex justify-center">
-        <h1>{title}</h1>
-      </div>
-      <nav className="w-full my-4 border-red-600 p-1">
-        <ul className="w-inherit">
-          {links &&
-            links.map((link, index) => {
-              return (
-                <li key={index} className="w-inherit flex justify-center">
-                  <ToggleList>
-                    <a href={link.href}>{link.text}</a>
-                  </ToggleList>
-                </li>
-              );
-            })}
-        </ul>
-      </nav>
-    </aside>
-  ) : (
-    <></>
+  const variants = {
+    hidden: { x: -260, opacity: 0 },
+    visible: { x: 0, opacity: 1 },
+    exit: { x: -260, opacity: 0 },
+  };
+  return (
+    <AnimatePresence>
+      {show && (
+        <motion.aside
+          key={"sidebar"}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          variants={variants}
+          transition={{ type: "tween", duration: 0.25 }}
+          className="min-w-60 flex flex-col items-center py-2 px-3"
+        >
+          <div className="w-full flex justify-center">
+            <h1>{title}</h1>
+          </div>
+          <nav className="w-full my-4 border-red-600 p-1">
+            <ul className="w-inherit">
+              {links &&
+                links.map((link, index) => {
+                  return (
+                    <li key={index} className="w-inherit flex justify-center">
+                      <ToggleList>
+                        <a href={link.href}>{link.text}</a>
+                      </ToggleList>
+                    </li>
+                  );
+                })}
+            </ul>
+          </nav>
+        </motion.aside>
+      )}
+    </AnimatePresence>
   );
 }
