27328c4062e3687698956d263a66c2a1a0c341be
Alter <jf.lozano.n@gmail.com>
2026-02-04 21:16:23 -0600

    feat: working on task system

---
+++ src/components/react/NewTask.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function NewTask({ }) {
+  const variants = {
+    
+  }
+  return (
+    <AnimatePresence>
+      <motion.section></motion.section>
+    </AnimatePresence>
+  );
+}
