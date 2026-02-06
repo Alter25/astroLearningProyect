09f015ada9a523ce63e166999a1c9acfa28466d0
Alter <jf.lozano.n@gmail.com>
2026-02-05 20:28:17 -0600

    components: starting with taskt & newTask

---
+++ src/components/react/NewTask.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
+export default function NewTask({}) {
+  const variants = {};
+  return (
+    <AnimatePresence>
+      <motion.section></motion.section>
+    </AnimatePresence>
+  );
+}
