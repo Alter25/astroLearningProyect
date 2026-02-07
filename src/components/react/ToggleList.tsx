4f9c57d5d2a18e052b5058327972f713c20eae22
Alter <jf.lozano.n@gmail.com>
2026-02-07 09:32:57 -0600

    bug: toggle actually show motion animation

--- src/components/react/ToggleList.tsx
+++ src/components/react/ToggleList.tsx
@@ -20,7 +20,7 @@ const TitleBlock = ({ title = "empty", icon }: blockProps) => {
   return (
     <div className="w-full h-8 flex justify-evenly">
       <span className="flex-1 ml-1">{title}</span>
-      <button onClick={() => icon?.f(!icon.value)}>
+      <button onClick={() => icon?.f((prev) => !prev)}>
         <span>
           <DropIcon show={icon?.value} />
         </span>
@@ -29,28 +29,35 @@ const TitleBlock = ({ title = "empty", icon }: blockProps) => {
   );
 };
 
+const animasVariants = {
+  hidden: {
+    opacity: 0,
+    height: 0,
+  },
+  visible: {
+    opacity: 1,
+    height: "auto",
+    transition: {
+      duration: 0.3,
+      ease: "easeInOut",
+    },
+  },
+};
+
 export default function ToggleList({ children, title }: toggleProps) {
   const [visible, setIsVisible] = useState(true);
-  const animas = {
-    hidden: {
-      opacity: 0,
-      height: 0,
-    },
-    visible: {
-      opacity: 1,
-      height: "auto",
-      transition: {
-        duration: 0.3,
-        ease: "easeInOut",
-      },
-    },
-  };
+
   return (
     <div className="w-full my-1 p-1">
       <TitleBlock title={title} icon={{ value: visible, f: setIsVisible }} />
       {visible && (
         <AnimatePresence>
-          <motion.div animate={animas} className="border">
+          <motion.div
+            variants={animasVariants}
+            initial="hidden"
+            animate="visible"
+            className="border"
+          >
             <div className="px-4 border border-red-100">{children}</div>
           </motion.div>
         </AnimatePresence>
