97bae7086ddca34ac21b9001cde6389324638ab7
Alter25 <alter@LozPc.localdomain>
2026-02-20 16:31:40 -0600

    feature:Card component

---
+++ src/components/react/Card.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Card({ }) {
+  return <div className="border w-40 h-50 rounded-xl">
+    
+  </div>
+}
---
+++ src/components/react/TaskListWrapper.tsx
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+import Card from "./Card";
+
+
+export default function TaskListWrapper({}) {
+
+  return (<Card></Card>);
+}
