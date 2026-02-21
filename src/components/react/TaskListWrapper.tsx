57d3e9f98a338d055eb50cd61c8e317f81e32ca7
Alter25 <alter@LozPc.localdomain>
2026-02-21 01:13:07 -0600

    update

--- src/components/react/TaskListWrapper.tsx
+++ src/components/react/TaskListWrapper.tsx
@@ -1,40 +1,6 @@
 import {useState, useEffect } from "react";
 import Card from "./Card";
 
-interface Character {
-  id: number;
-  name: string;
-  status: 'Alive' | 'Dead' | 'unknown';
-  species: string;
-  type: string;
-  gender: 'Female' | 'Male' | 'Genderless' | 'unknown';
-  origin: {
-    name: string;
-    url: string;
-  };
-  location: {
-    name: string;
-    url: string;
-  };
-  image: string;
-  episode: string[];
-  url: string;
-  created: string;
-}
-
-interface ApiResponse {
-  info: {
-    count: number;
-    pages: number;
-    next: string | null;
-    prev: string | null;
-  };
-  results: Character[];
-}
-
-interface ApiProps {
-  info?: ApiResponse;
-}
 
 const ApiUrl = 'https://rickandmortyapi.com/api/character';
 
